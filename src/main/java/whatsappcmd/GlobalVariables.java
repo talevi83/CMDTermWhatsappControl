838537209a573947ffdf05b763b557bbbebb74b8
Tal <talevi83@gmail.com>
2025-06-05 11:54:35 +0300

    Add artifacts and convert the config file to UTF-8 encoding. Also the contact to search in whatsapp to get messages from is now configurable via config.properties file.

--- src/main/java/whatsappcmd/GlobalVariables.java
+++ src/main/java/whatsappcmd/GlobalVariables.java
@@ -8,27 +8,7 @@ import java.util.Properties;
 
 public abstract class GlobalVariables {
 
-    protected static Properties properties;
-    static {
-        try {
-            properties = new Properties();
-
-            // load the config file in UTF-8 encoding.
-            InputStream inputStream = GlobalVariables.class.getClassLoader().getResourceAsStream("config.properties");
-            if (inputStream == null) {
-                throw new FileNotFoundException("Property file 'config.properties' not found in the classpath");
-            }
-
-            InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-            properties.load(reader);
-
-            reader.close();
-            inputStream.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    protected static Properties properties = loadPropertiesFile();
 
     protected static final String OS = System.getProperty("os.name");
     protected static final String HOME_DIRECTORY = System.getProperty("user.home");
@@ -40,4 +20,39 @@ public abstract class GlobalVariables {
     protected static String CMD_FLAG = OS.toLowerCase().contains("windows") ? "/c" : "-c";
 
     protected static WebDriver driver;
+
+    private static Properties loadPropertiesFile() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream;
+
+            // Load the properties file from the Jar directory.
+            File jarDir = new File(System.getProperty("java.class.path")).getAbsoluteFile().getParentFile();
+            File externalFile = new File(jarDir, "config.properties");
+
+            if (externalFile.exists()) {
+                inputStream = new FileInputStream(externalFile);
+                System.out.println("Loaded external config.properties");
+            } else {
+                // fallback: load the file from the Jar file.
+                inputStream = GlobalVariables.class.getClassLoader().getResourceAsStream("config.properties");
+                if (inputStream == null) {
+                    throw new FileNotFoundException("Property file 'config.properties' not found externally or in the JAR");
+                }
+                System.out.println("Loaded internal config.properties from JAR");
+            }
+
+            // Read the file with UTF-8 encoding.
+            InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            properties.load(reader);
+
+            reader.close();
+            inputStream.close();
+            return properties;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+
 }
