f989686e8beff0913ea5738b35626e4f75994ea1
Tal <talevi83@gmail.com>
2025-06-06 09:45:51 +0300

    Add password for mac to config file, Although it is not recommended to use sudo commands with your password with this program since the password is being sent unencrypted and is visible for sniffers and logs. Also added more shortcuts messages for sleep, shutdown and cancel shutdown. The shutdown on windows have a delay of 1 minutes (as designed) and you can cancel the shutdown during that minute with another message "cancel shutdown".

--- src/main/java/whatsappcmd/MessageWrapperConstants.java
+++ src/main/java/whatsappcmd/MessageWrapperConstants.java
@@ -7,6 +7,12 @@ public class MessageWrapperConstants {
                 return "curl https://api.ipify.org";
             case "internal ip":
                 return GlobalVariables.OS.toLowerCase().contains("windows") ? "for /f \"tokens=14 delims= \" %a in ('ipconfig ^| findstr \"IPv4\"') do @echo %a\n" : "ipconfig getifaddr en0";
+            case "sleep":
+                return "rundll32.exe powrprof.dll,SetSuspendState 0,1,0";
+            case "shutdown":
+                return GlobalVariables.OS.toLowerCase().contains("windows") ? "shutdown /s /f /t 60" : "echo '" + GlobalVariables.properties.get("mac.password") + "' | sudo -S shutdown -h now";
+            case "cancel shutdown":
+                return "shutdown /a";
         }
         return msg;
     }
